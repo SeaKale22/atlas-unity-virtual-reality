3141bba742f2c6d5a0bbe4b4bcd078688cb6d2f0
Kaleb K <kaleb.tahji.k@gmail.com>
2024-06-12 12:23:45 -0500

    Vignett settings

--- accessibility/Assets/Scripts/MainMenu.cs
+++ accessibility/Assets/Scripts/MainMenu.cs
@@ -10,14 +10,24 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenuCanvas;
     public GameObject optionsCanvas;
     public XRGazeInteractor GazeInteractor;
+    public GameObject vignetting;
 
     private float GazeSelectionTime;
+    private int VignettOn;
 
     public void Start()
     {
-        
         GazeSelectionTime = PlayerPrefs.GetFloat("GazeHoverTime", 1.5f);
         GazeInteractor.hoverTimeToSelect = GazeSelectionTime;
+        VignettOn = PlayerPrefs.GetInt("Vignetting", 0);
+        if (VignettOn == 1)
+        {
+            vignetting.SetActive(true);
+        }
+        else
+        {
+            vignetting.SetActive(false);
+        }
     }
 
     // method for loading scene
--- accessibility/Assets/Scripts/Options.cs
+++ accessibility/Assets/Scripts/Options.cs
@@ -15,7 +15,11 @@ public class Options : MonoBehaviour
     public GameObject optionsCanvas;
     public Slider GazeTimeSlider;
     public TMP_Text GazeTimeText;
+    public GameObject vignetting;
+    public Toggle VignettToggle;
+    
     private float GazeTimeSet;
+    private int VignettOn;
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,18 @@ public class Options : MonoBehaviour
         GazeTimeSlider.onValueChanged.AddListener(UpdateValue);
         GazeTimeText.text = $"Gaze Selection Time: {GazeTimeSlider.value} ";
         Apply();
+
+        VignettOn = PlayerPrefs.GetInt("Vignetting", 0);
+        if (VignettOn == 1)
+        {
+            vignetting.SetActive(true);
+            VignettToggle.isOn = true;
+        }
+        else
+        {
+            vignetting.SetActive(false);
+            VignettToggle.isOn = false;
+        }
     }
 
     // Update is called once per frame
@@ -37,11 +53,36 @@ public class Options : MonoBehaviour
     public void Apply()
     {
         GazeInteractor.hoverTimeToSelect = PlayerPrefs.GetFloat("GazeHoverTime");
+        
+        if (VignettOn == 1)
+        {
+            vignetting.SetActive(true);
+        }
+        else
+        {
+            vignetting.SetActive(false);
+        }
     }
     
     public void MainMenu()
     {
         mainMenuCanvas.SetActive(true);
         optionsCanvas.SetActive(false);
+    }
+
+    public void VignettOnOff()
+    {
+        if (VignettOn == 1)
+        {
+            PlayerPrefs.SetInt("Vignetting", 0);
+            VignettOn = 0;
+            vignetting.SetActive(false);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Vignetting", 1);
+            VignettOn = 1;
+            vignetting.SetActive(true);
+        }
     }
 }
